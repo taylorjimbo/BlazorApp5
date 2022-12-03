ebe38139700c5c2d9bb92a0da09fe092ca31ca46
jtaylor <jtaylor@JTAYLOR-WORK>
2022-12-02 18:12:12 -0600

    Add project files.

---
+++ BlazorApp5/Pages/ConnectionParametersPage.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebRTCme.Middleware;
+
+namespace BlazorApp5.Pages
+{
+    public partial class ConnectionParametersPage
+    {
+        [Inject]
+        ConnectionParametersViewModel ConnectionParametersViewModel { get; set; }
+
+        string[] _servernames { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            ConnectionParametersViewModel.OnPageAppearing(_servernames);
+            await base.OnInitializedAsync();
+        }
+
+        private void ReRender()
+        {
+            StateHasChanged();
+        }
+    }
+}
